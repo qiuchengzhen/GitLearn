39c14bdee6ad9fbb052470835757c8c6fcba8f3d
qiu <qiuchengzhen2008@163.com>
2024-08-29 21:36:55 +0800

    Initial commit

---
+++ GitForLearnLIUtieMeng/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace GitForLearnLIUtieMeng;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int[] nums1 = { 1, 2,3,4,5 };
+       ArrayList nums2 = new ArrayList{ 1,2,3,4,5 };
+       Console.WriteLine(Sum(nums1));
+       Console.WriteLine(Avg(nums1));
+      // Console.WriteLine(Sum((int[])nums2));
+    }
+
+    static int Sum(int[] nums)
+    {
+        int sum = 0;
+        foreach (var num in nums) sum += num;
+        return sum;
+    }
+
+    static double Avg(int[] nums)
+    {
+        int sum = 0;
+        double count = 0;
+        foreach (var num in nums) { sum += num; count++; }
+        return (double)sum / count;
+    }
+
+}
